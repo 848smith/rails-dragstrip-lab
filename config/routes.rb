e9f962dcbe12a329c1cbccbff7267b4fca138846
848smith <848smith@gmail.com>
2020-07-12 15:08:53 -0400

    edited the routes to make sure they follow the methods in the current controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :vehicles
-  resources :tracks
-  resources :races
-  resources :drivers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'drivers#home'
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  resources :drivers, only: [:new, :create, :show]
 end
