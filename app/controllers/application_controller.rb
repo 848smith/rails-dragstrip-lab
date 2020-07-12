97d829e788a764725082f5a1371f43ed94f4984d
848smith <848smith@gmail.com>
2020-07-12 15:01:26 -0400

    added in helper methods to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    helper_method :driver_layout, :require_login
+
+    def driver_layout
+      @driver = Driver.find_by(id: session[:driver_id])
+    end
+  
+    def require_login
+      redirect_to '/' unless session.include? :driver_id
+    end
 end
