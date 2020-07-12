ad5f28c5ddc33426a6b1b1a5f0d8f30243ef92ec
848smith <848smith@gmail.com>
2020-07-12 15:03:14 -0400

    added in the methods to the driver controller. A home, show, new and create method that also uses a params that permits data.

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,28 @@
 class DriversController < ApplicationController
+    layout 'application'
+    before_action :driver_layout, only: [:home]
+    before_action :require_login, only: [:show]
+
+    def home
+    end
+
+    def new
+        @driver = Driver.new
+    end
+
+    def create
+        @driver = Driver.create(driver_params)
+        session[:driver_id] = @driver.id
+        redirect_to driver_path(@driver)
+    end
+    
+    def show
+        @driver = Driver.find_by(id: params[:id])
+    end
+
+    private
+
+    def driver_params
+        params.require(:driver).permit(:name, :password, :age, :experience, :country)
+    end
 end
