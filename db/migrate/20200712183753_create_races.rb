538eacd303ed4a7b71325e22e9d0701ad61493c8
848smith <848smith@gmail.com>
2020-07-12 14:38:33 -0400

    added in controller, db migrations and model for Races.

---
+++ db/migrate/20200712183753_create_races.rb
@@ -0,0 +1,12 @@
+class CreateRaces < ActiveRecord::Migration[6.0]
+  def change
+    create_table :races do |t|
+      t.integer :driver_id
+      t.integer :track_id
+      t.integer :track_time
+      t.boolean :winner
+
+      t.timestamps
+    end
+  end
+end
