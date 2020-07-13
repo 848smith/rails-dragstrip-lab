77a4d044927a37861b2fff8cb403bfbcf9ee9f86
848smith <848smith@gmail.com>
2020-07-13 03:12:17 -0400

    created the races controller and built out the new method and race_params. Also added validation for the Race model.

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,2 +1,18 @@
 class RacesController < ApplicationController
+    layout "application"
+    before_action :driver_layout
+
+    def new
+        @race = Race.new
+    end
+
+    def create
+
+    end
+
+    private
+
+    def race_params
+        params.require(:race).permit(:track_time)
+    end
 end
--- app/models/race.rb
+++ app/models/race.rb
@@ -1,4 +1,5 @@
 class Race < ApplicationRecord
     belongs_to :driver
     belongs_to :track
+    validates :track_time, presence: true
 end
